80894677a436966216302150c6eb3133f674c82e
Adrien CARBONARO <adrien@orosound.com>
2022-03-26 22:32:27 +0100

    fixup update blog

---
+++ src/assets/ImagesBretagne.js
@@ -0,0 +1,55 @@
+import img_bretagne_1          from '../assets/images/bretagne/img-bretagne-1.jpg';
+import img_bretagne_2          from '../assets/images/bretagne/img-bretagne-2.jpg';
+import img_bretagne_3          from '../assets/images/bretagne/img-bretagne-3.jpg';
+import img_bretagne_5          from '../assets/images/bretagne/img-bretagne-5.jpg';
+import img_bretagne_6          from '../assets/images/bretagne/img-bretagne-6.jpg';
+import img_bretagne_7          from '../assets/images/bretagne/img-bretagne-7.jpg';
+import img_bretagne_8          from '../assets/images/bretagne/img-bretagne-8.jpg';
+import img_bretagne_9          from '../assets/images/bretagne/img-bretagne-9.jpg';
+import img_bretagne_10         from '../assets/images/bretagne/img-bretagne-10.jpg';
+import img_bretagne_11         from '../assets/images/bretagne/img-bretagne-11.jpg';
+import img_bretagne_12         from '../assets/images/bretagne/img-bretagne-12.jpg';
+import img_bretagne_14         from '../assets/images/bretagne/img-bretagne-14.jpg';
+import img_bretagne_15         from '../assets/images/bretagne/img-bretagne-15.jpg';
+import img_bretagne_16         from '../assets/images/bretagne/img-bretagne-16.jpg';
+import img_bretagne_19         from '../assets/images/bretagne/img-bretagne-19.jpg';
+import img_bretagne_20         from '../assets/images/bretagne/img-bretagne-20.jpg';
+import img_bretagne_24         from '../assets/images/bretagne/img-bretagne-24.jpg';
+import img_bretagne_25         from '../assets/images/bretagne/img-bretagne-25.jpg';
+import img_bretagne_26         from '../assets/images/bretagne/img-bretagne-26.jpg';
+import img_bretagne_27         from '../assets/images/bretagne/img-bretagne-27.jpg';
+import img_bretagne_29         from '../assets/images/bretagne/img-bretagne-29.jpg';
+import img_bretagne_30         from '../assets/images/bretagne/img-bretagne-30.jpg';
+import img_bretagne_32         from '../assets/images/bretagne/img-bretagne-32.jpg';
+import img_bretagne_33         from '../assets/images/bretagne/img-bretagne-33.jpg';
+import img_bretagne_34         from '../assets/images/bretagne/img-bretagne-34.jpg';
+import img_bretagne_35         from '../assets/images/bretagne/img-bretagne-35.jpg';
+
+export const ImagesBretagne = {
+	img_bretagne_1,
+	img_bretagne_2,
+	img_bretagne_3,
+	img_bretagne_5,
+	img_bretagne_6,
+	img_bretagne_7,
+	img_bretagne_8,
+	img_bretagne_9,
+	img_bretagne_10,
+	img_bretagne_11,
+	img_bretagne_12,
+	img_bretagne_14,
+	img_bretagne_15,
+	img_bretagne_16,
+	img_bretagne_19,
+	img_bretagne_20,
+	img_bretagne_24,
+	img_bretagne_25,
+	img_bretagne_26,
+	img_bretagne_27,
+	img_bretagne_29,
+	img_bretagne_30,
+	img_bretagne_32,
+	img_bretagne_33,
+	img_bretagne_34,
+	img_bretagne_35,
+}
