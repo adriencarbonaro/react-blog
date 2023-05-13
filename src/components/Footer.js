93edc89e66b048fb58edaf0d520990933106f5a2
Adrien CARBONARO <carbonaro.adrien@gmail.com>
2023-05-13 14:33:41 +0200

    fix(footer): remove empty space"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,5 @@
 import React, { Component }  from 'react';
-import { Button }            from './Button';
-import { Link }              from 'react-router-dom';
 import { Version }           from './Version';
-
-/* Assets */
-import { Strings }           from '../assets/Strings';
 
 /* CSS */
 import '../css/Footer.css';
@@ -12,17 +7,20 @@ import '../css/Footer.css';
 class Footer extends Component {
 	render() {
 		return (
+			<>
+			<div className='footer-margin'></div>
 			<div className='footer-container'>
 				<section class='social-media'>
 					<div class='social-media-wrap'>
 						<small class='website-rights'>Fait avec <i className="fa fa-heart"></i> par Camille & Adrien</small>
-						<a class='social-icon-link' target='_blank' href='https://www.instagram.com/le_petit_bonnet_rose/'>
+						<a class='social-icon-link' rel="noopener noreferrer" target='_blank' href='https://www.instagram.com/le_petit_bonnet_rose/'>
 							<i class='fab fa-instagram' />
 						</a>
 						<small class='website-version'><Version /></small>
 					</div>
 				</section>
 			</div>
+			</>
 		);
 	}
 }
